1b9ecffd92fa59de3257831d528703ed9513cc64
Jeff <jcavejr@gmail.com>
2018-05-08 23:37:42 -0400

    added checks for everything before creating stuff

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,23 +3,27 @@
 ##################
 # Find installer #
 ##################
-#INSTALL=apt install
-#command -v apt >/dev/null 2>&1 || INSTALL="pacman -Su"
-#echo $INSTALL
-INSTALL="sudo pacman -Su"
+command -v pacman >/dev/null 2>&1 && INSTALL="sudo pacman -S --noconfirm"
+command -v apt >/dev/null 2>&1 && INSTALL="sudo apt -y install"
+command -v zypper >/dev/null 2>&1 && INSTALL="sudo zypper in -y"
 
 ###############################
 # Check for required programs #
 ###############################
-#command -v git >/dev/null 2>&1 || sudo $INSTALL git
-#command -v vim >/dev/null 2>&1 || sudo $INSTALL vim
-#command -v ssh >/dev/null 2>&1 || sudo $INSTALL openssh-client
-#command -v gcc >/dev/null 2>&1 || sudo $INSTALL gcc
+command -v git >/dev/null 2>&1 || $INSTALL git 2>/dev/null
+command -v vim >/dev/null 2>&1 || $INSTALL vim 2>/dev/null
+command -v ssh >/dev/null 2>&1 || $INSTALL openssh-client 2>/dev/null
+command -v gcc >/dev/null 2>&1 || $INSTALL gcc 2>/dev/null
+command -v deluge >/dev/null 2>&1 || $INSTALL deluge 2>/dev/null
+command -v opencad >/dev/null 2>&1 || $INSTALL opencad 2>/dev/null
+command -v blender >/dev/null 2>&1 || $INSTALL blender 2>/dev/null
+command -v gimp >/dev/null 2>&1 || $INSTALL gimp 2>/dev/null
+command -v keepass >/dev/null 2>&1 || $INSTALL keepass 2>/dev/null
 
 #################
 # Setup SSH Key #
 #################
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
+if [ ! -f ~/.ssh/id_rsa.pub ]; then #checks if ssh key already exists before creating
     ssh-keygen -t rsa
     cat ~/.ssh/id_rsa.pub | ssh jtc178@clamshell.rutgers.edu 'cat >> .ssh/authorized_keys'
     echo "Add the following to github"
@@ -29,43 +33,85 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
     read -p "Press [enter] to continue..."
 fi
 
-####################
+#################### can possibly write this way more efficiently with loops and python..
 # Setup school dir #
 ####################
-mkdir ~/school
-mkdir ~/school/spring18
-mkdir ~/school/spring18/os
-git clone git@github.com:jcavejr/cs341.git ~/school/spring18/os
-mkdir ~/school/spring18/networks
-git clone git@github.com:jcavejr/cs446.git ~/school/spring18/networks
-mkdir ~/school/spring18/compgeo
-mkdir ~/school/spring18/algorithms
-mkdir ~/school/spring18/philanthropy
-mkdir ~/school/fall17
-mkdir ~/school/fall17/candsystems
-git clone git@github.com:jcavejr/cs211.git ~/school/fall17/candsystems
-mkdir ~/school/fall17/comporg
-git clone git@github.com:jcavejr/cs331.git ~/school/fall17/comporg
-mkdir ~/school/spring17
-mkdir ~/school/spring17/objectoriented
-git clone git@github.com:jcavejr/cs113.git ~/school/spring17/objectoriented
+if [ ! -d ~/school ]; then
+    mkdir ~/school
+fi
+if [ ! -d ~/school/spring18 ]; then
+    mkdir ~/school/spring18
+fi
+if [ ! -d ~/school/os ]; then
+    mkdir ~/school/spring18/os
+    git clone git@github.com:jcavejr/cs341.git ~/school/spring18/os
+fi
+if [ ! -d ~/school/spring18/networks ]; then
+    mkdir ~/school/spring18/networks
+    git clone git@github.com:jcavejr/cs446.git ~/school/spring18/networks
+fi
+if [ ! -d ~/school/spring18/compgeo ]; then
+    mkdir ~/school/spring18/compgeo
+fi
+if [ ! -d ~/school/spring18/algorithms ]; then
+    mkdir ~/school/spring18/algorithms
+fi
+if [ ! -d ~/school/spring18/philanthropy ]; then
+    mkdir ~/school/spring18/philanthropy
+fi
+if [ ! -d ~/school/fall17 ]; then
+    mkdir ~/school/fall17
+fi
+if [ ! -d ~/school/fall17/candsystems ]; then
+    mkdir ~/school/fall17/candsystems
+    git clone git@github.com:jcavejr/cs211.git ~/school/fall17/candsystems
+fi
+if [ ! -d ~/school/fall17/comporg ]; then
+    mkdir ~/school/fall17/comporg
+    git clone git@github.com:jcavejr/cs331.git ~/school/fall17/comporg
+fi
+if [ ! -d ~/school/spring17 ]; then
+    mkdir ~/school/spring17
+fi
+if [ ! -d ~/school/spring17/objectoriented ]; then
+    mkdir ~/school/spring17/objectoriented
+    git clone git@github.com:jcavejr/cs113.git ~/school/spring17/objectoriented
+fi
+
 
 ###################
 # Clone git repos #
 ###################
 cd ~/repos
-git clone git@github.com:jcavejr/ramaswamiresearch.git
-git clone git@github.com:jcavejr/encryption.git
-git clone git@github.com:jcavejr/perpdiscordbot.git
-git clone git@github.com:jcavejr/pypong.git
-git clone git@github.com:jcavejr/dot_randomizer.git
-git clone git@github.com:jcavejr/clip_acute_angles.git
-git clone git@github.com:jcavejr/weightlifted.git
-git clone https://github.com/iridakos/goto.git
+if [ ! -d ~/repos/ramaswami_research ]; then
+    git clone git@github.com:jcavejr/ramaswami_research.git
+fi
+if [ ! -d ~/repos/encryption ]; then
+    git clone git@github.com:jcavejr/encryption.git
+fi
+if [ ! -d ~/repos/perpdiscordbot ]; then
+    git clone git@github.com:jcavejr/perpdiscordbot.git
+fi
+if [ ! -d ~/repos/pypong ]; then
+    git clone git@github.com:jcavejr/pypong.git
+fi
+if [ ! -d ~/repos/dot_randomizer ]; then
+    git clone git@github.com:jcavejr/dot_randomizer.git
+fi
+if [ ! -d ~/repos/clip_acute_angles ]; then
+    git clone git@github.com:jcavejr/clip_acute_angles.git
+fi
+if [ ! -d ~/repos/weightlifted ]; then
+    git clone git@github.com:jcavejr/weightlifted.git
+fi
+if [ ! -d ~/repos/goto ]; then
+    git clone https://github.com/iridakos/goto.git
+fi
 
 ##################
 # Setup programs #
 ##################
-mkdir ~/bin
-cp ~/repos/dot_files_and_scripts/bin/* ~/bin/*
-$INSTALL deluge gimp blender
+if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+fi
+cp ~/repos/dot_files_and_scripts/bin/* ~/bin
